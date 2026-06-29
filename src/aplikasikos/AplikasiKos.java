db7d70cad606f2e4791caa45a7474b13eb5ef701
Tr1cky3 <anshorullah3@gmail.com>
2026-06-29 16:11:51 +0700

    Commit Awalan

---
+++ src/aplikasikos/AplikasiKos.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aplikasikos;
+
+import view.frameUtama;
+/**
+ *
+ * @author Tricky-Laptop
+ */
+public class AplikasiKos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        frameUtama view = new frameUtama();
+        view.setLocationRelativeTo(null);
+        view.setVisible(true);
+    }
+    
+}
